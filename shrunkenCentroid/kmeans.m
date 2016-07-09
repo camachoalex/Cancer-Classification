071b56e525d1f653dc51b2b9b8012a8b2a5fce3e
Alejandro Camacho <camacho.mail@gmail.com>
2016-07-09 13:29:32 -0700

    small edits

--- shrunkenCentroid/kmeans.m
+++
@@ -1,6 +0,0 @@
-addpath('cancer_data')
-% read data
-A = xlsread('imputed_prostate.xls'); %243 genes, 139 patiens
-A=A(2:end,:); % omit header
-% Cluster everything naively. k-means reads observations as rows. 
-[IDX, C, SUMD,D] = kmeans(A',100,'Distance','sqeuclidean');
